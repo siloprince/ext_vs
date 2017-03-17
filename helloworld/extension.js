2bd9420ba798c0232a3285f1b7eed2a8f8a4fcbc
Yoshiaki Araki <info@tessellation.jp>
2017-03-17 19:40:50 +0900

    simple

--- helloworld/extension.js
+++ helloworld/extension.js
@@ -1,30 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+'use strict';
+// load node_component/vscode
+// reference: https://code.visualstudio.com/docs/extensionAPI/vscode-api
+// download: 
+let vscode = require('vscode');
+exports.activate = activate;
+exports.deactivate = deactivate;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    console.log('Congratulations, your extension "test" is now active!');
+    context.subscriptions.push(vscode.commands.registerCommand('jp.tessellation.araki.family1', function() {
+        vscode.window.showInformationMessage('Hello World333!');
+        vscode.window.setStatusBarMessage('xxxxx', 10 * 1000);
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('jp.tessellation.araki.family2', function() {
+        vscode.window.showInformationMessage('Hello World4444!');
+    }));
 }
-exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+function deactivate() {}
